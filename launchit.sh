8da2c03068ae576bd36b4be7dc0ebcd7a48894b3
cst <cst@debian64-ssd>
2013-11-15 21:53:40 -0700

    better launchit.sh

--- launchit.sh
+++ launchit.sh
@@ -1,10 +1,31 @@
 #!/usr/bin/env bash
 
-function run_cmd {
-    gnome-terminal --window-with-profile=KeepOpen -x bash -l -c "$subcmd"
+source ~/.config/clamitrc
+
+GTERM_CONF=gnome-terminal-conf.xml
+GTERM_CONF_BAK=${CLAMIT_TMP}/${GTERM_CONF}
+
+
+function set_terminal {
+    [ ! -f "$GTERM_CONF_BAK" ] && \
+	gconftool-2 --dump '/apps/gnome-terminal' > $GTERM_CONF_BAK
+    gconftool-2 --load ${CLAMIT_SKEL}/${GTERM_CONF}
 }
 
-#cmd="gnome-terminal --window-with-profile=KeepOpen"
+function reset_terminal {
+    [ ! -f "$GTERM_CONF_BAK" ] && echo "Backup '${GTERM_CONF}' not found." && return
+    gconftool-2 --load $GTERM_CONF_BAK
+    rm $GTERM_CONF_BAK
+}
+
+function run_cmd {
+    set_terminal
+    gnome-terminal --window-with-profile=KeepOpen -x bash -l -c "$subcmd"
+    reset_terminal
+}
+
+
+### 'MAIN' function ###
 
 if [[ "$#" < "1" ]]; then
     exit
